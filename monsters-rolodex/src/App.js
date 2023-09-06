8bd32fa0c00df4cce3f7f9dc8af55cbe98b52ac3
manvisekhri08 <manvisekhri08@gmail.com>
2023-09-06 22:29:24 +0530

    setState concept

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,8 +6,12 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = {    //always a json object
-      name: 'React'
+    this.state = { 
+     //always a json object
+      firstname: 'Robert',
+      lastname: 'Langdon',
+      
+      comapny: 'Facebook'
     }
   }
   render() {
@@ -16,9 +20,18 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name} !!!
+            Hi {this.state.firstname} {this.state.lastname} and I work at {this.state.comapny} !!!
           </p>
-          <button onClick={() => this.setState({ name: 'Redux'})}>
+          <button onClick={ () => { this.setState(() => {
+            return {
+              firstname: 'Dan', lastname: 'Brown'
+            };
+          },
+          () => {
+            console.log(this.state)
+          }
+          ); //setState is an asynchronous call as console.log is synchrnous call and when its get called the state might not be updated as we saw in console on second click the state got updated in console so we will use function instead
+        }}>
             Change Name
           </button>
         </header>
