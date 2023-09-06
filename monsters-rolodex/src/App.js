7b10d2f0a1e8fc616e7fc6b5cd00451ca62c185a
manvisekhri08 <manvisekhri08@gmail.com>
2023-09-06 22:40:54 +0530

    mapping of elements

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,34 +7,31 @@ class App extends Component {
   constructor() {
     super();
     this.state = { 
+      monsters: [
+        {
+          name: 'Mike Wazowski'
+        },
+        {
+          name: 'Sulley'
+        },
+        {
+          name: 'Celia'
+        },
+        {
+          name: 'Frank Oz'
+        },
+      ]
      //always a json object
-      firstname: 'Robert',
-      lastname: 'Langdon',
-      
-      comapny: 'Facebook'
-    }
+    };
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.firstname} {this.state.lastname} and I work at {this.state.comapny} !!!
-          </p>
-          <button onClick={ () => { this.setState(() => {
-            return {
-              firstname: 'Dan', lastname: 'Brown'
-            };
-          },
-          () => {
-            console.log(this.state)
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>
+          })
           }
-          ); //setState is an asynchronous call as console.log is synchrnous call and when its get called the state might not be updated as we saw in console on second click the state got updated in console so we will use function instead
-        }}>
-            Change Name
-          </button>
-        </header>
       </div>
     );
   }
