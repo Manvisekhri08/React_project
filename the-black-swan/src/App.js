8dcb138418caecabfcb839a399e7b290bfb67533
manvisekhri08 <manvisekhri08@gmail.com>
2023-09-26 11:47:28 +0530

    Category component

--- the-black-swan/src/App.js
+++ the-black-swan/src/App.js
@@ -1,6 +1,8 @@
-import './categories.styles.scss'
+import CategoryItem from './components/category-item/category-item.component';
 
-const App = ()  => {
+import './categories.styles.scss';
+
+const App = () => {
   const categories = [
     {
       "id": 1,
@@ -28,19 +30,14 @@ const App = ()  => {
       "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
     }
   ]
+
   return (
-   <div className = "categories-container">
-    { categories.map(( {title, id, imageUrl } ) => (
-      <div key = {id} className = "category-container">
-        <div className = "background-image"  style={{ backgroundImage: `url(${imageUrl})` }} />
-      <div className = "category-body-container">
-        <h2>{title}</h2>
-        <p>Shop now</p>
-      </div>
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
-    ))}
-   </div>
   );
-}
+};
 
 export default App;
