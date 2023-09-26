87eae0c18f2b06bb27a634f242c2e8b00520f8a1
manvisekhri08 <manvisekhri08@gmail.com>
2023-09-26 17:27:25 +0530

    styling logo and nav bar

---
+++ the-black-swan/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link} from 'react-router-dom';
+import { Fragment } from 'react';
+import '../navigation/navigation.styles.scss';
+import { ReactComponent as SwanLogo } from '../../assets/crown.svg'
+
+const Navigation = () => {
+    return (
+      <Fragment>
+        <div className='navigation'>
+            <Link className='logo-container' to='/'>
+                <SwanLogo className='logo' />
+            </Link>
+          <div className='nav-links-container'>
+            <Link className='nav-link' to='/shop'>
+                SHOP
+            </Link>
+          </div>
+        </div>
+        <Outlet />
+      </Fragment>
+    )
+  }
+
+export default Navigation;
