f484b4eceedd4c87cdd88fa7d48b051a67ff43c1
manvisekhri08 <manvisekhri08@gmail.com>
2023-10-05 15:04:38 +0530

    sign in with redirect

--- the-black-swan/src/routes/sign-in/sign-in.component.jsx
+++ the-black-swan/src/routes/sign-in/sign-in.component.jsx
@@ -1,13 +1,25 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
 const SignIn = () => {
+
+    useEffect(() => {
+        (async () => {
+          const response = await getRedirectResult(auth);
+          if (response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+          }
+        })();
+      }, []);
     const logGoogleUser = async() => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
-    }
+    };
     return (
         <div>
             <h1>Hi I'm Sign In</h1>
             <button onClick={logGoogleUser}>Sign In With Google Pop Up</button>
+            <button onClick={signInWithGoogleRedirect}>Sign In With Google Redirect</button>
         </div>
     )
 }
