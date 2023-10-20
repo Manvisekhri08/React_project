eac12a90750f510e9af560aeca85f3ed98b00916
manvisekhri08 <manvisekhri08@gmail.com>
2023-10-20 18:51:36 +0530

    updating the cart

--- the-black-swan/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ the-black-swan/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import './cart-dropdown.styles.scss';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
-import { CartContext } from '../context/cart.context';
+import { CartContext } from '../context/cart.context'; 
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
