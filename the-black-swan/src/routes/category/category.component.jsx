0f68bcca4210ffcd874f003e7dca63ffeb6d09b3
manvisekhri08 <manvisekhri08@gmail.com>
2023-10-27 23:00:29 +0530

    fixing clashing css issues

--- the-black-swan/src/routes/category/category.component.jsx
+++ the-black-swan/src/routes/category/category.component.jsx
@@ -1,23 +1,28 @@
 import { useParams } from 'react-router-dom';
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useEffect, Fragment } from 'react';
 import { CategoriesContext } from '../../components/context/categories.context';
 import ProductCard from '../../components/product-card/product-card.component';
 import './category.styles.scss';
 
 const Category = () => {
-    const {category} = useParams();
-    const {categoriesMap} = useContext(CategoriesContext);
+    const { category } = useParams();
+    const { categoriesMap } = useContext(CategoriesContext);
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
         setProducts(categoriesMap[category])
     }, [category, categoriesMap])
     return (
-        <div className='category-container'>
-           { products &&
-           products.map((product) => <ProductCard key={product.id} product={product} />)
-           }
-        </div>
+        <Fragment>
+            <h2 className='category-title'>{category.toUpperCase()}
+                <div className='category-container'>
+                    {products &&
+                        products.map((product) => <ProductCard key={product.id} product={product} />)
+                    }
+                </div>
+            </h2>
+
+        </Fragment>
     )
 }
 
