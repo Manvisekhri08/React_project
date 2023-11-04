c65fb5b663f1ad65e6321b69b9e3e4c0ab28f54b
manvisekhri08 <manvisekhri08@gmail.com>
2023-11-04 13:46:51 +0530

    useNavigate to make categories responsive

--- the-black-swan/src/components/category-directory/category-directory.component.jsx
+++ the-black-swan/src/components/category-directory/category-directory.component.jsx
@@ -1,7 +1,40 @@
 import CategoryItem from "../category-item/category-item.component";
 import './category-directory.styles.scss'
 
-const CategoryDirectory = ({categories}) => {
+const categories = [
+  {
+    "id": 1,
+    "title": "Hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+     route:  'shop/hats'
+  },
+  {
+    "id": 2,
+    "title": "Jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    route:  'shop/jackets'
+  },
+  {
+    "id": 3,
+    "title": "Sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    route:  'shop/sneakers'
+  },
+  {
+    "id": 4,
+    "title": "Women",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    route:  'shop/women'
+  },
+  {
+    "id": 5,
+    "title": "Men",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    route:  'shop/men'
+  }
+]
+
+const CategoryDirectory = () => {
     return (
         <div className='categories-container'>
         {categories.map((category) => (
--- the-black-swan/src/components/category-item/category-item.component.jsx
+++ the-black-swan/src/components/category-item/category-item.component.jsx
@@ -1,9 +1,13 @@
 import {BackgroundImage, CategoryItemBodyContainer,  CategoryItemContainer } from './category-item.styles';
+import { useNavigate } from 'react-router-dom';
 
 const CategoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
   return (
-    <CategoryItemContainer>
+    <CategoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage
         imageUrl = {imageUrl}
       />
